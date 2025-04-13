4517d45ef726e174ccee9d14e5159239c83d63e7
asv-xyz <alex.sukhorukov123@yandex.ru>
2025-04-13 15:52:35 +0300

    Function commit

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 #include <unistd.h>
 #include <pthread.h>
 
+int value = 0;
+
+// thread function prototype
+void* routine();
+
 int main(int argc, char* argv[]) {
+    // thread declaration
+    pthread_t thread;
+    // result declaration
+    int* result;
+
+    // creating thread
+    if (pthread_create(&thread, NULL, &routine, NULL)) return EXIT_FAILURE;
+    if (pthread_join(thread, (void**)&result)) return EXIT_FAILURE;
+    printf("%d\n", *result);
+
     return EXIT_SUCCESS;
+}
+
+// thread function
+void* routine() {
+    value++;
+    int* result = malloc(sizeof(int));
+    *result = value;
+
+    return result;
 }
